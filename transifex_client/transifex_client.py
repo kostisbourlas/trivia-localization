785f95eec1f96444d8dfbc347f1b23a08655d324
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-04-09 10:51:25 +0300

    Add dotenv

--- transifex_client/transifex_client.py
+++ transifex_client/transifex_client.py
@@ -1,6 +1,16 @@
 # The file maps all the relevant Trasifex APIs.
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TRANSIFEX_SECRET_KEY = os.getenv('TRANSIFEX_SECRET_KEY')
 
 
 class TransifexClient:
     def upload_resource(self):
         return NotImplemented
+
+    def create_resource(self):
+        pass
