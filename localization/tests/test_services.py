74c2bcd13c0a35048479c9e92ef564412175b500
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-04-10 11:09:22 +0300

    Write tests for get_created_resources method

--- localization/tests/test_services.py
+++ localization/tests/test_services.py
@@ -2,7 +2,7 @@ from unittest import mock
 from django.test import TestCase
 
 from localization.objects import Resource
-from localization.service import get_or_create_resource
+from localization.service import get_or_create_resource, get_created_resources
 
 
 class GetOrCreateResourceTestCase(TestCase):
@@ -51,3 +51,43 @@ class GetOrCreateResourceTestCase(TestCase):
         # Assert that TransifexAPI.create_resource() was called with the correct
         # parameters
         mock_create_resource.assert_called_once_with(resource_name)
+
+
+class GetCreatedResourcesTestCase(TestCase):
+    @mock.patch('localization.service.TransifexAPI.get_all_resources')
+    def test_get_created_resources(self, mock_get_all_resources):
+        # Create mock data for the TransifexAPI.get_all_resources response
+        mock_data = {
+            "data": [
+                {
+                    "id": "123",
+                    "attributes": {
+                        "name": "Resource 1",
+                        "slug": "resource-1"
+                    }
+                },
+                {
+                    "id": "456",
+                    "attributes": {
+                        "name": "Resource 2",
+                        "slug": "resource-2"
+                    }
+                }
+            ]
+        }
+        mock_get_all_resources.return_value = mock_data
+
+        # Call the get_created_resources method and check the result
+        created_resources = get_created_resources()
+        self.assertEqual(len(created_resources), 2)
+
+    @mock.patch('localization.service.TransifexAPI.get_all_resources')
+    def test_get_created_resources_with_no_results(
+        self, mock_get_all_resources
+    ):
+        # Create mock data for the TransifexAPI.get_all_resources response
+        mock_data = {}
+        mock_get_all_resources.return_value = mock_data
+
+        created_resources = get_created_resources()
+        self.assertFalse(bool(created_resources))
