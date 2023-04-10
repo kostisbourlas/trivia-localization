ebbc1f9c3c7c0a7754bf596b0dff1e7476e01c86
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-04-10 17:29:14 +0300

    Add test for call_url_get

--- transifex_client/tests/test_utils.py
+++ transifex_client/tests/test_utils.py
@@ -1,6 +1,8 @@
+from unittest import mock
+
 from django.test import TestCase
 
-from transifex_client.utils import slugify_string
+from transifex_client.utils import slugify_string, call_url_get
 
 
 class SlugifyStringTest(TestCase):
@@ -18,3 +20,22 @@ class SlugifyStringTest(TestCase):
         input_string = "this-is-already-slugified"
         expected_output = "this-is-already-slugified"
         self.assertEqual(slugify_string(input_string), expected_output)
+
+
+class TestCallUrlGet(TestCase):
+    @mock.patch("requests.get")
+    def test_call_url_get(self, mock_requests_get):
+        # Mock the response of the get method
+        mock_response = mock.Mock()
+        mock_response.json.return_value = {"key": "value"}
+        mock_response.raise_for_status.return_value = None
+        mock_requests_get.return_value = mock_response
+
+        url = "https://superduperurl.com/api"
+        headers = {"User-Agent": "Test User Agent"}
+
+        response = call_url_get(url, headers)
+
+        mock_requests_get.assert_called_once_with(url, headers=headers)
+
+        self.assertDictEqual(response, {"key": "value"})
