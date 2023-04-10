682a466acde0a69c472cc7837da50897b0d0adb1
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-04-10 19:10:22 +0300

    Make transifex project env var

--- transifex_client/settings.py
+++ transifex_client/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TRANSIFEX_SECRET_KEY = os.getenv("TRANSIFEX_SECRET_KEY")
-TRANSIFEX_ORG_PROJECT = "o:koboucompany:p:assignment-trivia"
+TRANSIFEX_ORG_PROJECT = os.getenv("TRANSIFEX_ORG_PROJECT")
 
 # Trasifex URLS
 TRANSIFEX_BASE_URL = "https://rest.api.transifex.com"
