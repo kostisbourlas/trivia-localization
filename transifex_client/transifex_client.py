826fb1a72dda57249fb87b8f4dc334c06f9bc011
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-04-10 10:07:19 +0300

    Poll through endpoint for getting request download id

--- transifex_client/transifex_client.py
+++ transifex_client/transifex_client.py
@@ -1,4 +1,5 @@
 # The file maps all the relevant Trasifex APIs.
+from typing import Optional
 
 from transifex_client import settings
 from transifex_client.utils import (
@@ -43,25 +44,34 @@ class TransifexClient:
         return response
 
     def get_resource_data(self, resource_id) -> dict:
-        response: dict = self._request_resource_data(resource_id)
-        request_id: str = response.get("data").get("id")
-
+        request_id: Optional[str] = self._request_resource_download(resource_id)
         headers: dict = {
             "Authorization": f"Bearer {settings.TRANSIFEX_SECRET_KEY}",
         }
-        response: dict = call_url_get(
-            f"{settings.TRANSIFEX_REQUEST_RESOURCE_DATA_URL}/{request_id}",
-            headers
-        )
+        response: dict = {}
+        if request_id:
+            response: dict = call_url_get(
+                f"{settings.TRANSIFEX_REQUEST_RESOURCE_DATA_URL}/{request_id}",
+                headers
+            )
+
         return response
 
-    def _request_resource_data(self, resource_id) -> dict:
+    def _request_resource_download(self, resource_id) -> Optional[str]:
         headers: dict = {
             "Authorization": f"Bearer {settings.TRANSIFEX_SECRET_KEY}",
             "Content-Type": "application/vnd.api+json"
         }
         payload: dict = get_request_resource_data_payload(resource_id)
-        response: dict = call_url_post(
-            settings.TRANSIFEX_REQUEST_RESOURCE_DATA_URL, payload, headers
-        )
-        return response
+        retries: int = 5
+        while retries > 0:
+            response: dict = call_url_post(
+                settings.TRANSIFEX_REQUEST_RESOURCE_DATA_URL, payload, headers
+            )
+            retries -= 1
+            if response.get("data").get("attributes").get("status") == "succeeded":
+                return response.get("data").get("id")
+
+        # Assume that status is still pending which occurs when the resource
+        # has been created, but it contains no data.
+        return None
